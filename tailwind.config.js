93b21aa7b3d1caa9e4a53e18cc1b56cb596b20e0
SawantAchal <achals1718@gmail.com>
2024-10-23 17:22:48 +0530

    add animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'stroke': 'stroke 3s ease-in-out infinite',
+        'zoom': 'zoom 10s ease-in-out infinite',
+      },
+      keyframes: {
+        stroke: {
+          '0%, 100%': { 'stroke-dashoffset': '1000' },
+          '50%': { 'stroke-dashoffset': '0' },
+        },
+        zoom: {
+          '0%, 100%': { transform: 'scale(1)' },
+          '50%': { transform: 'scale(1.2)' }, 
+        },
+      },
+    },
   },
   plugins: [],
 }
